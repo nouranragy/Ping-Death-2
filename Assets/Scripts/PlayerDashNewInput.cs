20030e31324c3d1fb3a310f420d0fba2c49167d0
sondos6248 <sondosm284@gmail.com>
2026-08-11 02:43:24 +0300

    Player Animation 2

--- Assets/Scripts/PlayerDashNewInput.cs
+++ Assets/Scripts/PlayerDashNewInput.cs
@@ -86,7 +86,11 @@ public class PlayerDashNewInput : MonoBehaviour
         rb.gravityScale = originalGravity;
         rb.linearVelocity = Vector2.zero;
         isDashing = false;
-        if (anim != null) anim.SetBool("isDashing", false);
+        if (anim != null) 
+        {
+            anim.SetBool("isDashing", false);
+           
+        }
     }
 
     public void CancelDash()
@@ -102,12 +106,6 @@ public class PlayerDashNewInput : MonoBehaviour
          canDash = true;
 
     }
-
-        
-      
-       
-        // yield return new WaitForSeconds(dashCooldown);
-        
-    }
+}
 
 

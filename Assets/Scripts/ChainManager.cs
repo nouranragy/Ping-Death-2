d513bbee83a7340b0ee7a51ba4c68259e336b66d
nouranragy <nouranragy341@gmail.com>
2026-08-13 17:58:04 +0300

    add Level 3 dinosaur node attack & flee mechanics

--- Assets/Scripts/ChainManager.cs
+++ Assets/Scripts/ChainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class ChainManager : MonoBehaviour
 {
+    public static ChainManager Instance { get; private set; }
    [Header("Chain Settings")]
    public float timeWindow = 5.0f;
    private float currentTimer;
@@ -10,8 +11,19 @@ public class ChainManager : MonoBehaviour
 
    [Header("Nodes Configuraation")]
    public List<Node> activeNodeChain = new List<Node>();
-   private List<Node> unvisitedNodes = new List<Node>();
+   public List<Node> unvisitedNodes = new List<Node>();
    private Node currentTargetNode;
+
+   private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
    private void OnEnable()
     {
         Node.OnNodeConnected += ValidateConnection;
@@ -68,6 +80,28 @@ public class ChainManager : MonoBehaviour
             SetNextRandomTargetNode();
         }
     }
+
+    public void OnWrongNodeHit()
+    {
+        Debug.Log("Wrong Node Hit! Chain Broken!");
+        ResetChain();
+    }
+
+    public void DisconnectSingleNode(Node nodeToDisconnect)
+{
+    if (nodeToDisconnect == null) return;
+
+    Debug.Log($"Dinosaur disconnected Node: {nodeToDisconnect.nodeID}");
+
+ 
+    nodeToDisconnect.SetState(nodeToDisconnect.inactiveState);
+
+   
+    if (!unvisitedNodes.Contains(nodeToDisconnect))
+    {
+        unvisitedNodes.Add(nodeToDisconnect);
+    }
+}
     public void ResetChain()
     {
         isTimeRunning = false;
